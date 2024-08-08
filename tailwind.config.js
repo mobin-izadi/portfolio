946f96dfd24c8940abd4d7eeace1f8650f386bb0
mobin <mobinizadi.dev@gmail.com>
2024-08-08 22:30:08 +0330

    add font and config tailwind font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        vazirmatn: ['Vazirmatn', 'sans-serif'],
+      },
+      fontWeight: {
+        thin: 100,
+        extralight: 200,
+        light: 300,
+        normal: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+        extrabold: 800,
+        black: 900,
+      },
+    },
   },
   plugins: [],
 }

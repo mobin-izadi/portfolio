5aedfd4b1bc380d1565c633f76fc2135ac522151
mobin <mobinizadi.dev@gmail.com>
2024-08-08 23:13:51 +0330

    Add the logo and player to the header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
     extend: {
       fontFamily: {
         vazirmatn: ['Vazirmatn', 'sans-serif'],
